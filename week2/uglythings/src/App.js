6af240923e38d7d7eb818288aee173bb84d39d0f
rachelmaria09 <rmgildea@gmail.com>
2020-10-07 17:41:29 -0400

    first

--- week2/uglythings/src/App.js
+++ week2/uglythings/src/App.js
@@ -15,62 +15,62 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <h2 className="title">ToDo List</h2>
+        <h2 className="title">Pretty Things</h2>
         <ToDoContextConsumer>
           {({toDos, addToDo}) => (
-            <main>
-              <input
-                className="input"
-                type="text"
-                name="image"
-                placeholder="Image"
-                value={this.state.image}
-                onChange={this.handleChange}
-              />
-              <input
-                className="input"
-                type="text"
-                name="title"
-                placeholder="Title"
-                value={this.state.title}
-                onChange={this.handleChange}
-              />
-              <input
-                className="input"
-                type="text"
-                name="description"
-                placeholder="Why"
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
-              <br></br>
-              <button onClick={() => {
-                console.log(this.state)
-                this.setState(prevState => ({
-                  title: "",
-                  image: "",
-                  description: ""
-              }))
-                addToDo({
-                  title: this.state.title,
-                  image: this.state.image,
-                  description: this.state.description
-              })}}>Add!</button>
-              <br></br>
-              <div>
+        <main>
+        <input
+          className="input"
+          type="text"
+          name="image"
+          placeholder="Image"
+          value={this.state.image}
+          onChange={this.handleChange}
+        />
+        <input
+          className="input"
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <input
+          className="input"
+          type="text"
+          name="description"
+          placeholder="Why"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <br></br>
+        <button onClick={() => {
+          console.log(this.state)
+          this.setState(prevState => ({
+          title: "",
+          image: "",
+          description: ""
+        }))
+      addToDo({
+          title: this.state.title,
+          image: this.state.image,
+          description: this.state.description
+      })}}>Add!</button>
+          <br></br>
+          <div>
                 {/* not needed */}
                 {/* {({toDos}) => ( */}
-                  <ul>
-                    {toDos.map(toDo => 
-                    (<li style={{listStyle: "none"}}>
-                      <h2>{toDo.title}</h2>
-                      <p>{toDo.description}</p>
-                      <img src={toDo.image}></img>
-                    </li>))}
-                  </ul>
+          <ul>
+            {toDos.map(toDo => 
+            (<li style={{listStyle: "none"}}>
+            <h2>{toDo.title}</h2>
+            <p>{toDo.description}</p>
+            <img src={toDo.image}></img>
+            </li>))}
+          </ul>
                 {/* )} */}
-              </div>
-            </main>
+          </div>
+          </main>
           )}
         </ToDoContextConsumer>
       </div>
--- week2/uglythings/src/ToDoContext.js
+++ week2/uglythings/src/ToDoContext.js
@@ -4,10 +4,20 @@ const {Provider, Consumer} = React.createContext()
 
 class ToDoContextProvider extends React.Component {
     state = {
-        toDos: []
+        toDos: [
+            {title: "House",
+            description: "Beautiful",
+            image: "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"},
+            {title: "Car",
+            description: "Sporty",
+            image: "https://images.unsplash.com/photo-1537740547516-098bdb470e11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"},
+            {title: "Dog",
+            description: "Cute",
+            image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"}
+        ]
       }
 
-      addToDo = (toDo) => {
+    addToDo = (toDo) => {
         this.setState({toDos: [...this.state.toDos, toDo]})
     }
 
