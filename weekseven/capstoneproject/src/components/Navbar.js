6a1d8d0f28f72208c28dbfdfb74d6a032e159ac5
rachelmaria09 <rmgildea@gmail.com>
2020-11-10 16:50:31 -0500

    final

--- weekseven/capstoneproject/src/components/Navbar.js
+++ weekseven/capstoneproject/src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar (props) {
     return (
         <div>
         <div id="navBar">
+        <img id="logo" src="https://image.flaticon.com/icons/png/512/32/32904.png" />
             <Link id="link" to="/">Home</Link>
             <Link id="link" to="/facilities">Facilities</Link>
             <Link id="link" to="/gallery">Gallery</Link>
